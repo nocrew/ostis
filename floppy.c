c48241e48ec494d652f17a2069a87e490420721f
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-02-13 19:48:12 +0100

    Uncompressed MSA support

--- floppy.c
+++ floppy.c
@@ -4,6 +4,7 @@
 
 struct floppy {
   FILE *fp;
+  int inserted;
   int tracks;
   int sectors;
   int sides;
@@ -15,10 +16,12 @@ struct floppy {
 /* Currently only drive 0 (A:) supported */
 
 struct floppy floppy[2];
+static BYTE *floppy_raw_data;
+static LONG floppy_raw_data_size;
 
 void floppy_side(int side)
 {
-  if(!floppy[0].fp) return;
+  if(!floppy[0].inserted) return;
   if(side == 0)
     floppy[0].sel_side = 1;
   else
@@ -32,7 +35,7 @@ void floppy_active(int dmask)
 
 void floppy_sector(int sector)
 {
-  if(!floppy[0].fp) return;
+  if(!floppy[0].inserted) return;
   floppy[0].sel_sec = sector;
   if(sector < 1)
     floppy[0].sel_sec = 1;
@@ -42,7 +45,7 @@ void floppy_sector(int sector)
 
 int floppy_seek(int track)
 {
-  if(!floppy[0].fp) return FLOPPY_OK;
+  if(!floppy[0].inserted) return FLOPPY_OK;
   floppy[0].sel_trk = track;
   if(track < 0)
     floppy[0].sel_trk = 0;
@@ -60,7 +63,7 @@ int floppy_seek(int track)
 
 int floppy_seek_rel(int off)
 {
-  if(!floppy[0].fp) return FLOPPY_ERROR;
+  if(!floppy[0].inserted) return FLOPPY_ERROR;
   floppy[0].sel_trk += off;
   if(floppy[0].sel_trk < 0)
     floppy[0].sel_trk = 0;
@@ -76,10 +79,9 @@ int floppy_seek_rel(int off)
 
 int floppy_read_sector(LONG addr, int count)
 {
-  static BYTE sector[512];
   int pos,i,j;
 
-  if(!floppy[0].fp) return FLOPPY_ERROR;
+  if(!floppy[0].inserted) return FLOPPY_ERROR;
   if(floppy[0].sel_sec < 1) floppy[0].sel_sec = 1;
 
   pos = floppy[0].sel_trk*(floppy[0].sides+1)*floppy[0].sectors*512;
@@ -94,24 +96,27 @@ int floppy_read_sector(LONG addr, int count)
   printf(" = Pos:    %d\n", pos);
   printf("--------\n");
 #endif
-  fseek(floppy[0].fp, pos, SEEK_SET);
+
+
   for(i=0;i<count;i++) {
-    if(fread(sector, 512, 1, floppy[0].fp) != 1) return FLOPPY_ERROR;
+    if((pos+i*512) >= (floppy_raw_data_size-512)) return FLOPPY_ERROR;
     for(j=0;j<512;j++) {
-      mmu_write_byte(addr+i*512+j, sector[j]);
+      mmu_write_byte(addr+i*512+j, floppy_raw_data[pos+i*512+j]);
     }
   }
   return FLOPPY_OK;
 }
 
-void floppy_init(char *filename)
+BYTE *floppy_allocate_memory()
+{
+  return (BYTE *)malloc(86 * (floppy[0].sides+1) * floppy[0].sectors * 512);
+}
+
+void floppy_load_raw(FILE *fp)
 {
   static BYTE bootsector[512];
 
-  if(floppy[0].fp)
-    fclose(floppy[0].fp);
-  floppy[0].fp = fopen(filename, "rb");
-  if(!floppy[0].fp) return;
+  floppy[0].fp = fp;
 
   if(fread(bootsector, 512, 1, floppy[0].fp) != 1) {
     fclose(floppy[0].fp);
@@ -131,6 +136,28 @@ void floppy_init(char *filename)
   floppy[0].tracks = ((bootsector[19]|(bootsector[20]<<8))/
 		      (floppy[0].sides+1)/floppy[0].sectors);
 
+  // Now we load the entire floppy into memory
+  // Despite having fetched the track count, we'll allocate space for 86 tracks
+  floppy_raw_data = floppy_allocate_memory();
+  if(floppy_raw_data == NULL) {
+    printf("Unable to allocate floppy space\n");
+    return;
+  }
+
+  fseek(floppy[0].fp, 0, SEEK_END);
+  floppy_raw_data_size = ftell(floppy[0].fp);
+  rewind(floppy[0].fp);
+  if(fread(floppy_raw_data, 1, floppy_raw_data_size, floppy[0].fp) != floppy_raw_data_size) {
+    fclose(floppy[0].fp);
+    floppy[0].fp = NULL;
+    printf("Could not read entire file\n");
+    return;
+  }
+
+  fclose(floppy[0].fp);
+  floppy[0].fp = NULL;
+  floppy[0].inserted = 1;
+  
 #if 0
   printf("---FLOPPY LAYOUT---\n");
   printf(" - Tracks:  %d\n", floppy[0].tracks);
@@ -138,4 +165,104 @@ void floppy_init(char *filename)
   printf(" - Sectors: %d\n", floppy[0].sectors);
   printf("--------\n");
 #endif
+  
+}
+
+void floppy_load_msa(FILE *fp)
+{
+  BYTE msa_header[10];
+  int starting_track, ending_track;
+  int track,side,pos;
+  int track_size;
+  BYTE *track_data;
+  int track_data_size;
+
+  if(fread(msa_header, 10, 1, fp) != 1) {
+    fclose(fp);
+    return;
+  }
+
+  floppy[0].sectors = (msa_header[2]<<8)|msa_header[3];
+  floppy[0].sides = (msa_header[4]<<8)|msa_header[5];
+  starting_track = (msa_header[6]<<8)|msa_header[7];
+  if(starting_track != 0) {
+    printf("Error: MSA files not starting with track 0 are unsupported.\n");
+    return;
+  }
+  ending_track = (msa_header[8]<<8)|msa_header[9];
+  floppy[0].tracks = ending_track - starting_track + 1;
+
+#if 1
+  printf("---FLOPPY LAYOUT---\n");
+  printf(" - Tracks:  %d\n", floppy[0].tracks);
+  printf(" - Sides:   %d\n", floppy[0].sides+1);
+  printf(" - Sectors: %d\n", floppy[0].sectors);
+  printf("--------\n");
+#endif
+
+  track_size = floppy[0].sectors * 512;
+  
+  track_data = (BYTE *)malloc(track_size);
+  if(track_data == NULL) {
+    printf("Error: Unable to allocate track space\n");
+    return;
+  }
+
+  floppy_raw_data = floppy_allocate_memory();
+  if(floppy_raw_data == NULL) {
+    printf("Unable to allocate floppy space\n");
+    return;
+  }
+
+  floppy_raw_data_size = (floppy[0].sectors * 512) * floppy[0].tracks * (floppy[0].sides + 1);
+
+  pos = 0;
+
+  for(track = 0; track < floppy[0].tracks; track++) {
+    for(side = 0; side <= floppy[0].sides; side++) {
+      if(fread(track_data, 1, 2, fp) != 2) {
+        printf("Error: Unable to read track data\n");
+        return;
+      }
+      track_data_size = (track_data[0]<<8)|track_data[1];
+      if(track_data_size != track_size) {
+        printf("Error: Compressed MSA not supported\n");
+        return;
+      }
+      if(fread(track_data, 1, track_data_size, fp) != track_data_size) {
+        printf("Error: Unable to read track data\n");
+        return;
+      }
+      memcpy(&floppy_raw_data[pos], track_data, track_data_size);
+      pos += track_size;
+    }
+  }
+
+  floppy[0].inserted = 1;
+  return;
+}
+
+void floppy_init(char *filename)
+{
+  BYTE header[512];
+
+  floppy[0].inserted = 0;
+  
+  FILE *fp = fopen(filename, "rb");
+  if(!fp) return;
+
+  if(fread(header, 512, 1, fp) != 1) {
+    fclose(fp);
+    return;
+  }
+
+  rewind(fp);
+
+  // If header starts with 0x0e0f we treat this as an MSA file,
+  // otherwise it's considered to be a raw image.
+  if(header[0] == 0x0e && header[1] == 0x0f) {
+    floppy_load_msa(fp);
+  } else {
+    floppy_load_raw(fp);
+  }
 }
